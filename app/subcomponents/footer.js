2c25af9337ec20faf7458ff23c40e9cb3768dc0f
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-03-13 14:55:08 -0600

    test

--- app/subcomponents/footer.js
+++ app/subcomponents/footer.js
@@ -1,34 +1,50 @@
 import React from "react";
 
 const developers = [
-  { id: 1, name: "Rommel Hipos", imageUrl: "https://via.placeholder.com/150" },
+  { id: 1, name: "Rommel Hipos", imageUrl: "/assets/mel.png" },
   {
     id: 2,
     name: "Romeo Costillas",
-    imageUrl: "https://via.placeholder.com/150",
+    imageUrl: "/assets/romeo.png",
   },
-  { id: 2, name: "Ko Kogawichi", imageUrl: "https://via.placeholder.com/150" },
+  { id: 3, name: "Ko Kogawichi", imageUrl: "https://via.placeholder.com/150" },
   {
-    id: 2,
+    id: 4,
     name: "Aivee Madrelejos",
-    imageUrl: "https://via.placeholder.com/150",
+    imageUrl: "/assets/aivee.png",
   },
-  // Add more developers as needed
 ];
+
+const handleDeveloperClick = (developer) => {
+  console.log(developer.name);
+  if (developer.name === "Rommel Hipos") {
+    window.open("https:syntaxmelworks.com", "_blank");
+  }
+};
 
 const Footer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
       {developers.map((developer) => (
-        <div key={developer.id} className="flex border p-4 rounded-md">
-          <img
-            src={developer.imageUrl}
-            alt={developer.name}
-            className="w-16 h-16 rounded-full mr-4"
-          />
-          <div>
-            <p className="font-semibold">{developer.name}</p>
-            <p className="text-sm">{/* Add developer details here */}</p>
+        <div
+          key={developer.id}
+          className="developer-container transform transition-transform hover:-translate-y-1 shadow-md hover:shadow-lg"
+        >
+          <div
+            className="developer-content bg-white p-4 rounded-md cursor-pointer flex items-center"
+            onClick={() => handleDeveloperClick(developer)}
+          >
+            <img
+              src={developer.imageUrl}
+              alt={developer.name}
+              className="w-16 h-16 rounded-full mr-4"
+            />
+            <div>
+              <p className="font-semibold text-black italic">
+                {developer.name}
+              </p>
+              {/* <p className="text-sm">{}</p> */}
+            </div>
           </div>
         </div>
       ))}
