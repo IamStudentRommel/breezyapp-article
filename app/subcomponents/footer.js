249463c0c4d8a4a4bd54f96a761ba9f368a4191e
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-03-13 18:15:10 -0600

    test animate

--- app/subcomponents/footer.js
+++ app/subcomponents/footer.js
@@ -1,22 +1,8 @@
-import React from "react";
-
-const developers = [
-  { id: 1, name: "Rommel Hipos", imageUrl: "/assets/mel.png" },
-  {
-    id: 2,
-    name: "Romeo Costillas",
-    imageUrl: "/assets/romeo.png",
-  },
-  { id: 3, name: "Ko Kogawichi", imageUrl: "https://via.placeholder.com/150" },
-  {
-    id: 4,
-    name: "Aivee Madrelejos",
-    imageUrl: "/assets/aivee.png",
-  },
-];
+import React, { useState } from "react";
+import Developers from "../data/developers";
 
 const handleDeveloperClick = (developer) => {
-  console.log(developer.name);
+  // console.log(developer.name);
   if (developer.name === "Rommel Hipos") {
     window.open("https://syntaxmelworks.com", "_blank");
   } else {
@@ -25,27 +11,45 @@ const handleDeveloperClick = (developer) => {
 };
 
 const Footer = () => {
+  const [hoveredDeveloper, setHoveredDeveloper] = useState(null);
+
+  const handleMouseEnter = (developer) => {
+    setHoveredDeveloper(developer);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredDeveloper(null);
+  };
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
-      {developers.map((developer) => (
+      {Developers.map((developer) => (
         <div
           key={developer.id}
           className="developer-container transform transition-transform hover:-translate-y-1 shadow-md hover:shadow-lg"
+          onMouseEnter={() => handleMouseEnter(developer)}
+          onMouseLeave={handleMouseLeave}
         >
-          <div
-            className="developer-content bg-white p-4 rounded-md cursor-pointer flex items-center"
-            onClick={() => handleDeveloperClick(developer)}
-          >
+          <div className="developer-content bg-white p-4 rounded-md cursor-pointer flex items-center">
             <img
               src={developer.imageUrl}
               alt={developer.name}
-              className="w-16 h-16 rounded-full mr-4"
+              className={`w-16 h-16 rounded-full mr-4 ${
+                hoveredDeveloper === developer ? "flip" : ""
+              }`}
             />
             <div>
-              <p className="font-semibold text-black italic">
-                {developer.name}
-              </p>
-              {/* <p className="text-sm">{}</p> */}
+              {hoveredDeveloper === developer ? (
+                <a
+                  href="#" // Replace "#" with the actual link
+                  className="font-semibold text-blue-500 italic hover:underline"
+                >
+                  View Profile
+                </a>
+              ) : (
+                <p className="font-semibold text-black italic">
+                  {developer.name}
+                </p>
+              )}
             </div>
           </div>
         </div>
