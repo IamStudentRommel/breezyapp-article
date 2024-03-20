a592621bf77cc8755657913603a2f0d6695d5df7
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-03-20 09:22:25 -0600

    test adjust height

--- app/components/title.js
+++ app/components/title.js
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <div className="text-center md:text-2xl pt-12 md:pt-0 lg:pt-12 mb-8">
+    <div className="text-center md:text-2xl pt-12 md:pt-0 lg:pt-5 mb-8">
       <h1 className="text-2xl md:text-4xl  text-green-500 font-bold">
         Amazing Crime App
       </h1>
