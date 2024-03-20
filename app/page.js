911f2f13555da7565673361db1a3df0a2e46aa1a
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-03-20 09:15:04 -0600

    z

--- app/page.js
+++ app/page.js
@@ -111,29 +111,31 @@ const Page = () => {
     <main className="bg-gradient-to-br from-black via-gray-700 to-black text-white">
       <nav className="bg-black-500 p-4">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <div>
+          {/* <div>
             <img src="/assets/crimehate.png" alt="Logo" className="h-0" />
-          </div>
+          </div> */}
           {/* Burger menu icon for mobile */}
-          <div className="block md:hidden">
-            <button
-              onClick={toggleMenu}
-              className="text-white focus:outline-none"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+          <div className="flex items-center">
+            <div className="block md:hidden">
+              <button
+                onClick={toggleMenu}
+                className="text-white focus:outline-none"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            </button>
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
           {/* Navigation links */}
           <div className="hidden md:flex justify-center space-x-4">
